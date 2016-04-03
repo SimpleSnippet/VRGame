9850a91a7439a26855f7126c79f9131ef5c64437
Rooopeswar Kommalapati <roopeshkom@gmail.com>
2016-04-03 07:33:47 -0400

    This is the raw Unity C# code that powers the app

---
+++ Source/Assets/Scripts/PlayerCon.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class PlayerCon : MonoBehaviour 
+{
+
+	public float speed;
+
+	Controller controller;
+	Frame frame;
+	HandList hands;
+	Hand firstHand;
+
+	private bool grabbing;
+
+	void Start ()
+	{
+		controller = new Controller ();
+		GetHands ();
+	}
+
+	void Update ()
+	{
+		if (!(Physics.Raycast (transform.position, Camera.main.transform.forward, 1.0f))) {
+			if (grabbing){
+				transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+		}
+	}
+
+	void LateUpdate()
+	{
+		GetHands ();
+	}
+
+	void GetHands()
+	{
+		frame = controller.Frame (); // this gets the current frame from the controller
+		hands = frame.Hands;
+		firstHand = hands [0];
+
+		if (firstHand.GrabStrength >= .2f) {
+			grabbing = true;
+		} 
+		else 
+		{
+			grabbing = false;
+		}	
+	}
+}
